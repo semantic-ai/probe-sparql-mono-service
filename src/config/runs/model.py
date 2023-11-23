3c3b7bee11d6dc8871a913796e14ede4fe9d13dc
jarnedemeulemeester <jarnedemeulemeester@gmail.com>
2023-11-23 11:59:56 +0100

    refactor: set pull_token default

--- src/config/runs/model.py
+++ src/config/runs/model.py
@@ -8,7 +8,7 @@ from ...enums import ModelType, TrainingFlavours
 class ModelConfig(Settings):
     type: ModelType = ModelType.EMBEDDING_REGULAR.value
     flavour: TrainingFlavours = TrainingFlavours.DISTIL_BERT.value
-    pull_token: str
+    pull_token: str = ""
 
     class Config():
         env_prefix = f"{CONFIG_PREFIX}model_"
